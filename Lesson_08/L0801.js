472e8fb22240f410c3bde4d0cf07941c5922b97d
ptupe <30857143+ptupe@users.noreply.github.com>
2022-05-26 11:27:48 +0530

    Update L0801.js

--- Lesson_08/L0801.js
+++ Lesson_08/L0801.js
@@ -10,3 +10,14 @@ var n = parseInt(iStr); // <<< parseInt(iStr) converts variable iStr to an integ
 gs.info(typeof n + ' n=' + n + '& n + 2 =' + (n+2));
 var f = parseFloat(iStr); // <<< parseFloat(iStr) converts variable iStr to a float
 gs.info(typeof f + ' f=' + f + '& f + 2.4 =' + (f+2.4));
+
+
+// Exercise - 3 string variables , print lengths, concatenate them, print length of new string
+var a = 'JavaScript';
+var b = 'with';
+var c = 'Chuck';
+gs.info(a.length +'+'+ b.length +'+'+ c.length);
+var conCat = a + b + c;
+gs.info(typeof conCat + ' conCat=' + conCat + ' & it\'s length is' + conCat.length);
+var conCatNew = a +'\n'+ b +'\n'+ c;
+gs.info(typeof conCatNew + ' conCatNew=\n' + conCatNew + ' & it\'s length is' + conCatNew.length);
