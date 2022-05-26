0b6a7b8cb8832eab3ba2ffd4c4bc88594660872f
ptupe <30857143+ptupe@users.noreply.github.com>
2022-05-26 10:12:57 +0530

    Update L0801.js

--- Lesson_08/L0801.js
+++ Lesson_08/L0801.js
@@ -2,9 +2,11 @@
 //  L08S01 - Changing and detecting variable types
 //
 var i = 5;
-var iStr = i.toString();
+var iStr = i.toString(); // <<< .toString converts variable i to a string
 gs.info(typeof i);
-gs.info(typeof iStr);
+gs.info(typeof iStr); 
 
-var n = parseInt(iStr);
-gs.info(typeof n + ' n=' + n);
+var n = parseInt(iStr); // <<< parseInt(iStr) converts variable iStr to an integer
+gs.info(typeof n + ' n=' + n + '& n + 2 =' + (n+2));
+var f = parseFloat(iStr); // <<< parseFloat(iStr) converts variable iStr to a float
+gs.info(typeof f + ' f=' + f + '& f + 2.4 =' + (f+2.4));
