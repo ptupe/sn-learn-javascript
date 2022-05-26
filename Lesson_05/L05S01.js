bf1f0ec08a8c156e44e2874cdf7529eb6508f92f
ptupe <30857143+ptupe@users.noreply.github.com>
2022-05-26 09:28:17 +0530

    Update L05S01.js

--- Lesson_05/L05S01.js
+++ Lesson_05/L05S01.js
@@ -1,5 +1,6 @@
 //
 // L05S01 - Common error messages
 //
-gs.info(myUnknownVariable);
-ga.info('Hello, world!);
+gs.info(myUnknownVariable); //Need to declare a variable
+//Also check line above the line which shows an error message
+ga.info('Hello, world!); //gs is a build in library that we are using for JavaScript
