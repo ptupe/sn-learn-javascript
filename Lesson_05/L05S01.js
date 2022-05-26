cbf47a7134dbe45ba924e7a9868d7e4ef9532863
ptupe <30857143+ptupe@users.noreply.github.com>
2022-05-26 09:32:34 +0530

    Update L05S01.js

--- Lesson_05/L05S01.js
+++ Lesson_05/L05S01.js
@@ -3,4 +3,13 @@
 //
 gs.info(myUnknownVariable); //Need to declare a variable
 //Also check line above the line which shows an error message
-ga.info('Hello, world!); //gs is a build in library that we are using for JavaScript
+ga.info('Hello, world!); 
+//gs is a build in library that we are using for JavaScript
+        
+        
+//Exercise - perform 3 + 2 * 5 which should show result 25
+var a = 3;
+var b = 2;
+var c = 5;
+gs.info((3 + 2) * 5); // We can use multiple paranthesis inside gs.info
+gs.info((a + b) * c);
