dbdf0c7d65a1e9cb38d9b849024ac98b7c1335b2
ptupe <30857143+ptupe@users.noreply.github.com>
2022-05-26 09:58:12 +0530

    Update L07S01.js

--- Lesson_07/L07S01.js
+++ Lesson_07/L07S01.js
@@ -1,5 +1,5 @@
 // 
-// L07S01 - Special characters - called as Escape characters
+// L07S01 - Special characters - called as Escape characters when used a backslash
 //
 // \n = new line
 // \t = tab
@@ -10,3 +10,5 @@
 gs.info('Single string\nTwo lines');
 gs.info('Chuck\'s simple script');
 gs.info('Don\'t confuse a forward slash (/) with a backslash(\\)');
+// "/" is forward slash used in http://
+// "\" is a backslash used for escape characters in JavaScript 
